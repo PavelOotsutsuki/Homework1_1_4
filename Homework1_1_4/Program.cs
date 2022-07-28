64d87493c32406b58bbdf1dd95fdf936354cc2a7
PavelOotsutsuki <109985069+PavelOotsutsuki@users.noreply.github.com>
2022-07-28 16:01:01 +0300

    Добавьте файлы проекта.

---
+++ Homework1_1_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int columns = 3; // Количество столбцов
+            int image = 52; // Количество картинок
+            int rows; // Количество рядов
+            int imageRemain; // Кол-во изображений сверх меры
+
+            rows = image / columns;
+            imageRemain = image % columns;
+
+            Console.WriteLine("Можно будет вывести "+ rows+ " полностью заполненных рядов, " + imageRemain + " картинок будет сверх меры.");
+
+        }
+    }
+}
+
+
+
